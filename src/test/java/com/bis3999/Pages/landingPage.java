9c1abf5aef2e6ffdcaaabeb6ec25c7db5fe9f47a
Leung <michael.a.leung@accenture.com>
2017-05-18 17:57:52 +0800

    Add Maven JUnit Test Case

---
+++ src/test/java/com/bis3999/Pages/landingPage.java
@@ -0,0 +1,64 @@
+package com.bis3999.Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class landingPage {
+
+    @FindBy(linkText = "i am a link")
+    private WebElement theActiveLink;
+
+    @FindBy(id = "your_comments")
+    private WebElement yourCommentsSpan;
+
+    @FindBy(id = "comments")
+    private WebElement commentsTextAreaInput;
+
+    @FindBy(id = "submit")
+    private WebElement submitButton;
+
+    public WebDriver driver;
+    public static String url = "http://ec2-52-77-255-171.ap-southeast-1.compute.amazonaws.com:8080/bis3999";
+
+    public static landingPage visitPage(WebDriver driver) {
+        landingPage page = new landingPage(driver);
+        page.visitPage();
+        return page;
+    }
+
+    public landingPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void visitPage() {
+        this.driver.get(url);
+    }
+
+    public void followLink() {
+        this.theActiveLink.click();
+    }
+
+    public void submitComment(String text) {
+        this.commentsTextAreaInput.sendKeys(text);
+        this.submitButton.click();
+
+        // Race condition for time to populate yourCommentsSpan
+        WebDriverWait wait = new WebDriverWait(this.driver, 15);
+        wait.until(ExpectedConditions.textToBePresentInElement(yourCommentsSpan, text));
+    }
+
+    public String getSubmittedCommentText() {
+        return this.yourCommentsSpan.getText();
+    }
+
+    public boolean isOnPage() {
+        String title = "I am a page title - Sauce Labs";
+        return this.driver.getTitle() == title;
+    }
+
+}
